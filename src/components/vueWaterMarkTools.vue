59ff455b836b5c61a135e36b4b612b194a75e2e7
wuhaoran0409 <978434783@qq.com>
2018-08-20 16:27:33 +0800

    Update vueWaterMarkTools.vue

--- src/components/vueWaterMarkTools.vue
+++ src/components/vueWaterMarkTools.vue
@@ -5,7 +5,7 @@
             2.props其他属性可以均为使用者自定义属性,均有默认值。
             3.在父页面引入此组件之后需使用者在父页面中的mounted()钩子监并且在watch钩子中监听$route
             4.在父页面使用详细:
-                <water-mark ref="waterMarkRef" :fatherId="'app-main'" :txt="'金融数据网关'"></water-mark>
+                <water-mark ref="waterMarkRef" :fatherId="'app-main'" :txt="'水印文本'"></water-mark>
                 watch: {
                     $route() {
                         setTimeout(_ => {
