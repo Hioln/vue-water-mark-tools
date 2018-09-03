2810f169e53564ea23e9b6185cbb9f2296ec6a52
v_wuhaoran_dxm <v_wuhaoran@duxiaoman.com>
2018-09-03 16:54:49 +0800

    1.增加 - 监听页面DOM监听 - 防止水印标签被开发者删除

--- src/components/vueWaterMarkTools.vue
+++ src/components/vueWaterMarkTools.vue
@@ -5,7 +5,7 @@
             2.props其他属性可以均为使用者自定义属性,均有默认值。
             3.在父页面引入此组件之后需使用者在父页面中的mounted()钩子监并且在watch钩子中监听$route
             4.在父页面使用详细:
-                <water-mark ref="waterMarkRef" :fatherId="'app-main'" :txt="'水印文本'"></water-mark>
+                <water-mark ref="waterMarkRef" :fatherId="'app-main'" :txt="'金融数据网关'"></water-mark>
                 watch: {
                     $route() {
                         setTimeout(_ => {
@@ -19,12 +19,12 @@
             5.如果其他页面变动频繁
                 this.$store.state.waterMark.waterRef.initPage();
     -->
-    <div id="layerDiv"></div>
+    <div></div>
 </template>
 
 <script>
 export default {
-    name: 'v-water-mark',
+    name: 'WaterMark',
     data() {
         return {
             mainWidth: 0, // 用户页面的宽度
@@ -91,6 +91,7 @@ export default {
     },
     mounted() {
         //  根据浏览器窗口大小，水印自动适应窗口布局
+        this.observe();
         const that = this;
         window.onresize = () => {
             return (() => {
@@ -99,27 +100,49 @@ export default {
         };
     },
     methods: {
+        // 实时监听DOM变化，防止用户去除水印
+        observe() {
+            this.$log('document.body', document.body);
+            let bodyEl = document.body;
+            let that = this;
+            bodyEl.addEventListener('DOMNodeRemoved', function (event) {
+                that.initPage();
+            }, false);
+        },
         // 初始化页面
         initPage() {
             setTimeout(_ => {
+                this.$log('1.', 'initPage');
                 let layerDiv = document.getElementById('layerDiv');
                 //  判断是否存在最外层的div
                 if (layerDiv) {
-                    layerDiv.parentNode.removeChild(layerDiv);
-                    this.setDomData();
+                    this.$log('1-1', 'layerDiv存在');
+                    this.uploadCanvas();
                 } else {
+                    this.$log('1-2', 'layerDiv不存在');
                     this.setDomData();
                 }
             }, this.timeOut);
         },
+        //  如果存在canvas，将重新改变canvas的高度及宽度
+        uploadCanvas() {
+            this.setDomData('uploadCanvas');
+        },
         // 设置一些基本的属性数据
-        setDomData() {
-            this.mainWidth = document.getElementById(this.fatherId).offsetWidth;
+        setDomData(type) {
+            this.$log('2.', 'setDomData');
+            this.mainWidth = (_ => {
+                if (document.getElementById(this.fatherId)) {
+                    return document.getElementById(this.fatherId).offsetWidth;
+                }
+            })();
             this.mainHigh = (_ => {
-                if (document.getElementById(this.fatherId).offsetHeight < 500) {
-                    return 500;
-                } else {
-                    return document.getElementById(this.fatherId).offsetHeight;
+                if (document.getElementById(this.fatherId)) {
+                    if (document.getElementById(this.fatherId).offsetHeight < 500) {
+                        return 500;
+                    } else {
+                        return document.getElementById(this.fatherId).offsetHeight;
+                    }
                 }
             })();
             this.layerWidth = this.mainWidth;
@@ -128,48 +151,86 @@ export default {
             this.smallDivHigh = this.mainHigh / this.rowNum;
             this.smallCanvasWidth = this.mainWidth / this.colNum;
             this.smallCanvasHigh = this.mainHigh / this.rowNum;
-            let layerDiv = document.createElement('div');
-            layerDiv.setAttribute('id', 'layerDiv');
-            layerDiv.setAttribute('style', 'position:absolute;z-index:999999;top:0px;pointer-events:none;opacity: ' + this.opacity + ';overflow:hidden;');
-            layerDiv.style.width = this.layerWidth + 'px';
-            layerDiv.style.height = this.layerHigh + 'px';
-            document.getElementById(this.fatherId).appendChild(layerDiv);
-            this.createdDiv();
+            if (type !== 'uploadCanvas') {
+                let layerDiv = document.createElement('div');
+                layerDiv.setAttribute('id', 'layerDiv');
+                layerDiv.setAttribute('style', 'position:absolute;z-index:999999;top:0px;pointer-events:none;opacity: ' + this.opacity + ';overflow:hidden;');
+                layerDiv.style.width = this.layerWidth + 'px';
+                layerDiv.style.height = this.layerHigh + 'px';
+                if (document.getElementById(this.fatherId)) {
+                    document.getElementById(this.fatherId).appendChild(layerDiv);
+                }
+            } else {
+                let layerDiv = document.getElementById('layerDiv');
+                layerDiv.style.width = this.layerWidth + 'px';
+                layerDiv.style.height = this.layerHigh + 'px';
+            }
+            this.createdDiv(type);
         },
         // 创建小的div来包含小的canvas
-        createdDiv() {
-            for (let i = 1; i <= this.rowNum * this.colNum; i++) {
-                let div = document.createElement('div');
-                div.setAttribute('id', 'smallDiv' + i);
-                div.setAttribute('style', 'float: left;');
-                div.style.width = this.smallDivWidth + 'px';
-                div.style.height = this.smallDivHigh + 'px';
-                document.getElementById('layerDiv').appendChild(div);
+        createdDiv(type) {
+            if (type !== 'uploadCanvas') {
+                for (let i = 1; i <= this.rowNum * this.colNum; i++) {
+                    let div = document.createElement('div');
+                    div.setAttribute('id', 'smallDiv' + i);
+                    div.setAttribute('style', 'float: left;');
+                    div.style.width = this.smallDivWidth + 'px';
+                    div.style.height = this.smallDivHigh + 'px';
+                    if (document.getElementById('layerDiv')) {
+                        document.getElementById('layerDiv').appendChild(div);
+                    }
+                }
+                this.createdCanvas();
+            } else {
+                for (let i = 1; i <= this.rowNum * this.colNum; i++) {
+                    let divUpload = document.getElementById('smallDiv' + i);
+                    divUpload.style.width = this.smallDivWidth + 'px';
+                    divUpload.style.height = this.smallDivHigh + 'px';
+                }
+                this.createdCanvas(type);
             }
-            this.createdCanvas();
         },
         // 创建小的canvas来画水印
-        createdCanvas() {
-            for (let i = 1; i <= this.rowNum * this.colNum; i++) {
-                let canvas = document.createElement('canvas');
-                document.getElementById('smallDiv' + i).appendChild(canvas);
-                canvas.setAttribute('id', 'canvas' + i);
-                canvas.width = this.smallCanvasWidth;
-                canvas.height = this.smallCanvasHigh;
-                let ctx = canvas.getContext('2d');
-                ctx.beginPath();
-                ctx.rotate(this.angle * this.deg);
-                ctx.font = this.font;
-                ctx[this.lineType](this.txt, this.waterMarkSpace, this.waterMarkTop);
-                ctx[this.lineColorType] = this.fontColor;
-                ctx.closePath();
-                //  this.canvasToImage(canvas);
+        createdCanvas(type) {
+            if (type !== 'uploadCanvas') {
+                for (let i = 1; i <= this.rowNum * this.colNum; i++) {
+                    if (document.getElementById('smallDiv' + i)) {
+                        let canvas = document.createElement('canvas');
+                        document.getElementById('smallDiv' + i).appendChild(canvas);
+                        canvas.setAttribute('id', 'canvas' + i);
+                        canvas.width = this.smallCanvasWidth;
+                        canvas.height = this.smallCanvasHigh;
+                        let ctx = canvas.getContext('2d');
+                        ctx.beginPath();
+                        ctx.rotate(this.angle * this.deg);
+                        ctx.font = this.font;
+                        ctx[this.lineType](this.txt, this.waterMarkSpace, this.waterMarkTop);
+                        ctx[this.lineColorType] = this.fontColor;
+                        ctx.closePath();
+                    }
+                    //  this.canvasToImage(canvas);
+                }
+            } else {
+                for (let i = 1; i <= this.rowNum * this.colNum; i++) {
+                    let canvas = document.getElementById('canvas' + i);
+                    canvas.width = this.smallCanvasWidth;
+                    canvas.height = this.smallCanvasHigh;
+                    let ctx = canvas.getContext('2d');
+                    ctx.beginPath();
+                    ctx.rotate(this.angle * this.deg);
+                    ctx.font = this.font;
+                    ctx[this.lineType](this.txt, this.waterMarkSpace, this.waterMarkTop);
+                    ctx[this.lineColorType] = this.fontColor;
+                    ctx.closePath();
+                    //  this.canvasToImage(canvas);
+                }
             }
         },
         //  把canvas转化成base64格式的方法
         canvasToImage(canvas) {
             let image = new Image();
             image.src = canvas.toDataURL('image/png');
+            this.$log('base64', image.src);
         }
     }
 };
